9f40965bb68e5a5b212d8708beeea70933819f63
Blu <bludeleon@gmail.com>
2015-06-13 13:24:12 +0800

    challenge done

--- js.js
+++ js.js
@@ -1,23 +1,31 @@
  $(document).ready(function(){
 
+
+ 	$(".fizzButton").click(function(){
+
+ 		var	x = +$("input").val();
+ 		fizzBuzzPrint(x);
+ 	})
  	
-	var prev;
-	for( var i = 1; i < 101; i++){
-		if (i % 3 == 0 && i % 5 == 0){
-			//prev = $("div").text();
-			$("div").append("FizzBuzz"+"<br>");
-		} else if( i %  3 == 0){
-			//prev = $("div").text();
-			$("div").append("Fizz"+"<br>");
-		} else if (i % 5 == 0){
-			//prev = $("div").text();
-			$("div").append("Buzz"+"<br>");
-		}else{
-			//prev = $("div").text();
-			$("div").append(i+"<br>");
-		}
-	}
+	
+
 	
 	
  });
 
+
+function fizzBuzzPrint( n ){
+	console.log(n);
+	for ( var i = 1; i <= n;i++){
+		console.log(i);
+		if( i % 3 ==0 && i % 5 == 0){
+			$(".container").append("FizzBuzz<br>");
+		}else if ( i % 3 == 0){
+			$(".container").append("Fizz<br>");
+		}else if ( i % 5 ==0){
+			$(".container").append("Buzz<br>");
+		}else {
+			$(".container").append(i + "<br>");
+		}
+	}
+}
